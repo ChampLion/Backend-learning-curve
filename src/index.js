56cc76922a0dc7ccc13510faad69d5b8fcfc8626
ChampLion <vishal16feb2006@gmail.com>
2026-06-24 21:02:15 +0530

    more Setup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import { connectDB } from "./db/index.js";
 import { DB_NAME } from "./constants.js";
 import dotenv from 'dotenv'
+import { app } from "./app.js";
 
 dotenv.config({
   path: './env'
 })
 
-connectDB()
+connectDB().then(()=> {
+  app.listen(process.env.PORT)
+  console.log(`running on port ${process.env.PORT}`);
+  
+}).catch((err)=> {
+  console.log("Connection Failed", err);
+  })
